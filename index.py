767eb203aee31792036ac29d89f49ac5d0d3e2db
masin <masin@masin.com>
2024-08-01 08:22:32 -0500

    masin

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('اوم')
-
+    
 
 app = ApplicationBuilder().token("7398078714:AAEM2FrYLu84uQG9ny1alnWk3JoLaEtuXjM").build()
 
