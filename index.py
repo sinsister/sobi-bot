f86b5f916ad660834a6bfc56d34d76a0403b3186
masin <masin@masin.com>
2024-08-01 08:20:13 -0500

    masin

---
+++ index.py
@@ -0,0 +1,11 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('اوم')
+
+
+app = ApplicationBuilder().token("7398078714:AAEM2FrYLu84uQG9ny1alnWk3JoLaEtuXjM").build()
+
+app.add_handler(CommandHandler("start", hello))
+
+app.run_polling()
